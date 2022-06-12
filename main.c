134ecfbbedf7a18a5e050c05d59f892f3fed3a41
ricajust <ricardojustino@fastmail.com>
2022-06-12 11:13:23 -0300

    {rda-silv}[poc] - WIP: create events handler and hooks

--- main.c
+++ main.c
@@ -6,34 +6,56 @@
 /*   By: rda-silv <rda-silv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 08:57:25 by rda-silv          #+#    #+#             */
-/*   Updated: 2022/06/12 09:29:44 by rda-silv         ###   ########.fr       */
+/*   Updated: 2022/06/12 10:41:41 by rda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <X11/keysym.h>
 #include <mlx.h>
 
 #define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 300
-
 #define MLX_ERROR 1
 
-int	main(void)
+typedef struct s_data
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+}	t_data;
 
-	mlx_ptr = mlx_init();
-	if (mlx_ptr == NULL)
+/* This function needs to exist, but it is useless for the moment */
+int	handle_no_event()
+{
+	return (0);
+}
+
+int	handle_input(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	return (0);
+}
+
+int	main(void)
+{
+	t_data	data;
+
+	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
 		return (MLX_ERROR);
-	win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "SHAZAM");
-	if (win_ptr == NULL)
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "SHAZAM");
+	if (data.win_ptr == NULL)
 	{
-		free(win_ptr);
+		free(data.win_ptr);
 		return (MLX_ERROR);
 	}
-	while (1);
-	mlx_destroy_window(mlx_ptr, win_ptr);
-	mlx_destroy_display(mlx_ptr);
-	free(mlx_ptr);
+	/*Setup hooks*/
+	mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data);
+	mlx_key_hook(data.win_ptr, &handle_input, &data);
+	mlx_loop(data.mlx_ptr);
+	/* we will exit the loop if there's no window left, and execute this code */
+	mlx_destroy_window(data.mlx_ptr, data.win_ptr);
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
 }
