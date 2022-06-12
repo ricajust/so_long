03b0f9dfca4cd1a03e42f277907c4733bbffc35d
ricajust <ricardojustino@fastmail.com>
2022-06-12 10:18:05 -0300

    {rda-silv}[poc] - WIP: create a window and first defensive code

--- main.c
+++ main.c
@@ -6,18 +6,34 @@
 /*   By: rda-silv <rda-silv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 08:57:25 by rda-silv          #+#    #+#             */
-/*   Updated: 2022/06/12 08:57:59 by rda-silv         ###   ########.fr       */
+/*   Updated: 2022/06/12 09:29:44 by rda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <stdlib.h>
 #include <mlx.h>
+
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 300
+
+#define MLX_ERROR 1
 
 int	main(void)
 {
 	void	*mlx_ptr;
+	void	*win_ptr;
 
 	mlx_ptr = mlx_init();
+	if (mlx_ptr == NULL)
+		return (MLX_ERROR);
+	win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "SHAZAM");
+	if (win_ptr == NULL)
+	{
+		free(win_ptr);
+		return (MLX_ERROR);
+	}
+	while (1);
+	mlx_destroy_window(mlx_ptr, win_ptr);
 	mlx_destroy_display(mlx_ptr);
 	free(mlx_ptr);
 }
