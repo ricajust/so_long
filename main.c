270fc409bcfd64819ba24a8de41ff2c6ce77d9f8
ricajust <rda-silv@Legion5i.localdomain>
2022-06-15 20:52:33 -0300

    {ricajus}[poc] - WIP: Drawing a red pixel

--- main.c
+++ main.c
@@ -6,43 +6,42 @@
 /*   By: rda-silv <rda-silv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 08:57:25 by rda-silv          #+#    #+#             */
-/*   Updated: 2022/06/12 21:46:37 by rda-silv         ###   ########.fr       */
+/*   Updated: 2022/06/15 20:49:09 by rda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <X11/X.h>
-#include <X11/keysym.h>
-#include <mlx.h>
+#include <X11/X.h> //Biblioteca para os metodos 'KeyPress' e 'KeyPressMask'
+#include <X11/keysym.h> //Biblioteca que mapeia o teclado
+#include <mlx.h> //minilibx
 
-#define WINDOW_WIDTH 600
-#define WINDOW_HEIGHT 300
-#define MLX_ERROR 1
+#define WINDOW_WIDTH 600 //Constante da largura da janela
+#define WINDOW_HEIGHT 300 //Constante da altura da janela
+#define MLX_ERROR 1 //Constante onde setamos o valor de retorno do erro
+#define RED_PIXEL 0xff0000 // Constante do valor do pixel, neste caso vermelho
 
 typedef struct s_data
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-}	t_data;
-
-/* This function needs to exist, but it is useless for the moment */
-int	handle_no_event()
-{
-	return (0);
-}
+	void	*mlx_ptr; //ponteiro que receberá o minilibx
+	void	*win_ptr; //ponteiro que receberá a janela
+}	t_data; 
 
 int	handle_keypress(int keysym, t_data *data)
 {
 	if (keysym == XK_Escape)
+	{
 		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-	printf("Keypress: %d\n", keysym);
+		data->win_ptr = NULL; //precisamos definir a janela como null após sua destruição
+	}
 	return (0);
 }
 
-int	handle_keyrelease(int keysym)
+int	render(t_data * data)
 {
-	printf("Keypress: %d\n", keysym);
+	if (data->win_ptr != NULL) // verifica se a janela ainda existe antes de colocar o pixel
+		mlx_pixel_put(data->mlx_ptr, data->win_ptr, 
+			WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, RED_PIXEL);
 	return (0);
 }
 
@@ -50,22 +49,25 @@ int	main(void)
 {
 	t_data	data;
 
-	data.mlx_ptr = mlx_init();
+	data.mlx_ptr = mlx_init(); // inicia e atribui o minilibx
 	if (data.mlx_ptr == NULL)
-		return (MLX_ERROR);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "SHAZAM");
-	if (data.win_ptr == NULL)
+		return (MLX_ERROR); // se erro de conexão com o X server retorna erro
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "SHAZAM"); // cria a janela
+	if (data.win_ptr == NULL) //Testa se a atribuição da janela foi válida
 	{
-		free(data.win_ptr);
-		return (MLX_ERROR);
+		free(data.win_ptr); //Se não, libera memória...
+		return (MLX_ERROR); //...e retorna o erro (1)
 	}
-	/*Setup hooks*/
-	mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data);
+	/*Fazendo os setups dos hooks*/
+	mlx_loop_hook(data.mlx_ptr, &render, &data);
 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
-	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &handle_keyrelease, &data);
+	/**
+	 * @brief Função utilizada para receber eventos. Ela nunca retorna e é um 
+	 * loop infinito que aguardo um evento e em seguida chama uma função 
+	 * definida pelo usuário associada ao evento
+	 * @param mlx_ptr identificado de conexão 
+	 */
 	mlx_loop(data.mlx_ptr);
-	/* we will exit the loop if there's no window left, and execute this code */
-	mlx_destroy_window(data.mlx_ptr, data.win_ptr);
 	mlx_destroy_display(data.mlx_ptr);
 	free(data.mlx_ptr);
 }
