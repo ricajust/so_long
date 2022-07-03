30a935c1d86ac720a1e04083b2382a56a5d7678b
Ricajust <ricardojustino@fastmail.com>
2022-07-03 15:32:58 -0300

    {rda-silv}[so_long] - a functional makefile

---
+++ Makefile
@@ -0,0 +1,51 @@
+# LIBFT_PATH		=	./library/libft
+# LIBFT			=	$(LIBFT_PATH)/libft.a
+
+MINILIBX_PATH	=	./library/minilibx
+MINILIBX		=	$(MINILIBX_PATH)/libmlx.a
+
+SOURCE_FILES	=	main.c 
+
+SOURCE_DIR		=	source
+HEADER			=	$(SOURCE_DIR)/so_long.h
+
+SOURCES			=	$(addprefix $(SOURCE_DIR)/, $(SOURCE_FILES))
+
+OBJECTS			= 	$(SOURCES:.c=.o)
+
+NAME			=	so_long
+
+CC				=	gcc
+RM				=	rm -f
+
+CFLAGS			=	-Wall -Wextra -Werror
+MLXFLAGS		=	-L. -lXext -L. -lX11
+
+.c.o:
+				$(CC) $(CFLAGS) -c $< -o $(<:.c=.o)
+
+all:			$(NAME)
+
+# $(NAME):		$(LIBFT) $(MINILIBX) $(OBJECTS) $(HEADER)
+$(NAME):		$(MINILIBX) $(OBJECTS) $(HEADER)
+				$(CC) $(CFLAGS) $(OBJECTS) $(MINILIBX) $(MLXFLAGS) -o $(NAME)
+
+
+# $(LIBFT):
+# 				$(MAKE) -C $(LIBFT_PATH)
+
+$(MINILIBX):
+				$(MAKE) -C $(MINILIBX_PATH)
+
+clean:
+#				$(MAKE) -C $(LIBFT_PATH) clean
+				$(MAKE) -C $(MINILIBX_PATH) clean
+				$(RM) $(OBJECTS)
+
+fclean:			clean
+#				$(MAKE) -C $(LIBFT_PATH) fclean
+				$(RM) $(NAME)
+
+re:				fclean all
+
+.PHONY:			all clean fclean re libft minilibx bonus
--- source/main.c
+++ source/main.c
@@ -6,47 +6,11 @@
 /*   By: rda-silv <rda-silv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 08:57:25 by rda-silv          #+#    #+#             */
-/*   Updated: 2022/06/15 22:03:28 by rda-silv         ###   ########.fr       */
+/*   Updated: 2022/06/26 15:48:17 by rda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <X11/X.h> //Biblioteca para os metodos 'KeyPress' e 'KeyPressMask'
-#include <X11/keysym.h> //Biblioteca que mapeia o teclado
-#include <mlx.h> //minilibx
-
-#define WINDOW_WIDTH 600 //Constante da largura da janela
-#define WINDOW_HEIGHT 300 //Constante da altura da janela
-#define MLX_ERROR 1 //Constante onde setamos o valor de retorno do erro
-#define RED_PIXEL 0xff0000 // Constante do valor do pixel, neste caso vermelho
-#define GREEN_PIXEL 0xFF00// Constante do valor do retangulo, setado como verde
-#define WHITE_PIXEL 0xFFFFFF// Constante do valor do fundo, setado como branco
-
-typedef struct s_img
-{
-	void	*mlx_img;
-	char	*addr;
-	int		bpp; /*bit per pixel*/
-	int		line_len;
-	int		endian;
-}	t_img;
-
-typedef struct s_rect
-{
-	int	x;
-	int	y;
-	int	width;
-	int	height;
-	int	color;
-}	t_rect;
-
-typedef struct s_data
-{
-	void	*mlx_ptr; //ponteiro que receberá o minilibx
-	void	*win_ptr; //ponteiro que receberá a janela
-	t_img	img;
-}	t_data; 
+#include "so_long.h"
 
 int	handle_keypress(int keysym, t_data *data)
 {
